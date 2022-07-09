2d5ca9e52cf51a0e84f8ce7f0d916970c331c5e3
Ohm <49402476+kiloOhm@users.noreply.github.com>
2022-07-09 14:01:48 +0200

    video

--- src/views/pages/Media.vue
+++ src/views/pages/Media.vue
@@ -31,7 +31,7 @@ sliders.map(() => indices.push(ref(0)))
       class="card"
     >
       <iframe
-        src="https://www.youtube.com/embed/kkRkaxFFTeI" 
+        src="https://www.youtube.com/embed/iM9KTEOKP9w" 
         title="YouTube video player" 
         frameborder="0" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
